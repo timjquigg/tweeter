fbc53826a112a2e1031e259f1b82e3e473c3d578
Tim Quigg <timjquigg@gmail.com>
2022-11-09 19:38:13 -0700

    Added jQuerry AJAX requests to GET tweets from the server and POST new tweets to the server. There is also input validation to ensure tweets are between 0 and 140 characters long and XSS is prevented by using the jQuery .text method to add user text.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,76 +6,6 @@
 
 // Fake data taken from initial-tweets.json
 
-const convertDate = (date) => {
-  const minute = 1000 * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const year = day * 365;
-
-  const now = new Date().getTime();
-  const difference = now - date;
-  
-  if (difference > 2 * year) {
-    return `${Math.floor(difference / year)} years ago`;
-  }
-
-  if (difference > year) {
-    return '1 year ago';
-  }
-
-  if (difference > 2 * day) {
-    return `${Math.floor(difference / day)} days ago`;
-  }
-
-  if (difference > day) {
-    return '1 day ago';
-  }
-
-  if (difference > 2 * hour) {
-    return `${Math.floor(difference / hour)} hours ago`;
-  }
-
-  if (difference > hour) {
-    return '1 hour ago';
-  }
-
-  if (difference > 2 * minute) {
-    return `${Math.floor(difference / hour)} minutes ago`;
-  }
-
-  if (difference > minute) {
-    return '1 minute ago';
-  }
-
-  return `${Math.floor(difference / 1000)} seconds ago`;
-
-};
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const renderTweets = function(tweets) {
 // loops through tweets
   for (const tweet in tweets) {
@@ -105,14 +35,16 @@ const createTweetElement = function(tweet) {
   $header.append($userDiv).append($userHandle);
 
   // Tweet content
-  const $tweetBody = $(`<p>${tweet.content.text}</p>`);
+  const $tweetBody = $('<p></p>');
+  $tweetBody.text(tweet.content.text);
 
   // Footer
   const $footer = $('<footer></footer>');
 
-  
-  const $date = $(`<p>${convertDate(tweet.created_at)}</p>`);
+  // Date
+  const $date = $(`<p>${timeago.format(tweet.created_at)}</p>`);
 
+  // Flag, Retweet, Heart
   const $iconsDiv = $('<div class="icons"></div>');
   const $flag = $('<i class="fa-solid fa-flag"></i>');
   const $reTweet = $('<i class="fa-solid fa-retweet"></i>');
@@ -120,14 +52,52 @@ const createTweetElement = function(tweet) {
 
   $iconsDiv.append($flag).append($reTweet).append($heart);
 
+  // Complete Footer
   $footer.append($date).append($iconsDiv);
 
+  // Complete Article
   $tweet.append($header).append($tweetBody).append($footer);
 
   return $tweet;
 };
 
-$(document).ready(function() {
+const handleSubmit = function(event) {
+  event.preventDefault();
+  const tweet = $(this).find('#tweet-text');
 
-  renderTweets(data);
+  if (tweet.val() === '') {
+    alert('Cannot submit and empty tweet!');
+    return;
+  }
+
+  if (tweet.val().length > 140) {
+    alert('Tweet length cannot exceed 140 characters!');
+    return;
+  }
+
+  $.ajax({
+    data: $(this).serialize(),
+    url: '/tweets',
+    method: 'POST',
+    success: ()=>{
+      $(tweet).val('');
+      loadTweets();
+    }
+  });
+
+};
+
+const loadTweets = function() {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+  }).then(renderTweets);
+};
+
+$(document).ready(function() {
+ 
+  loadTweets();
+
+  $('.new-tweet > form').on('submit', handleSubmit);
+
 });
